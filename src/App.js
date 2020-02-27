14912922d7884305cc4a072de7f6eaa9e0d26976
EMIRATES\S905521 <lehaandile.quangdiem@dnata.com>
2020-02-27 13:00:56 +0400

    clinic 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import AirportDropdown from './components/airportdropdown.js';
+import LegsDialog from './components/legsdialog';
 import './App.css';
+import Button from './components/Button/button.js';
 
-function AgeCalculator(props){
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {legs:[],legsInStr: ""};
+    this.saveLegs = this.saveLegs.bind(this);
+  }
+  toggleSelected(id, key){
+    let temp = this.state[key]
+    temp[id].selected = !temp[id].selected
+    this.setState({
+      [key]: temp
+    })
+  };
+  
+  componentDidMount(){
+    document.querySelector('#legs').style.display = "none";
+  }
+  
+  editHandler = () => {
+    document.querySelector('#legs').style.display = "";
+  }
+  concatLegs = function(legs){
+    var ret = "";
+    if(legs) {
+      ret = legs.join("-");
+    }
+    return ret;
+  }
+  saveLegs = (legs) => {
+    const c = this.concatLegs(this.state.legs);
+    this.setState(
+      {legs: legs,
+        legsInStr: c
+      }
+    );
+    document.querySelector('#legs').style.display = "none";
+  }
+  
+  render() {
+    return (
+      <div  class="App-legs">
+        <div>{this.state.legsInStr} </div>
+        <div><Button text="Edit" handlerClick={this.editHandler} className="redbutton"/></div>
+        <div id="legs" ><LegsDialog legs={this.state.legs} saveLegs={this.saveLegs} /></div>
+      </div>
+    );
+  }
+}
+export const AgeCalculator = (props) => {
   return(
     <div>Age: {new Date().getFullYear() - props.yearOfBirth}</div>    
   );
 }
-export default AgeCalculator;
+export default App;
+
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AgeCalculator from './App.js';
-//import App from './App';
+
+import App, {AgeCalculator} from './App.js';
+
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AgeCalculator yearOfBirth="1978" />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
